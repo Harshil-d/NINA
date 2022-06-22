f21aa52f76d4415801d6722fbf124b852f037eb6
Harshil-d <zadafiyaharshil@gmail.com>
2022-06-22 12:19:04 +0530

    made changes for test moode.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
+  if (process.env.NODE_ENV === 'test') {
+    setTimeout(() => {
+      app.close();
+    }, 5000);
+  }
 }
 
 bootstrap();
