07bc8b4e0177cfe602844ca3a263847e11a8d58d
Harshil-d <zadafiyaharshil@gmail.com>
2022-06-24 14:50:26 +0530

    fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppControllerasdfas } from './app.controller';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
-}) aas
+})
 export class AppModule {}
